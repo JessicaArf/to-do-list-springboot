9392e5ac45e6ffa8ee3d2b11af691807ea3b55c7
Jessica Arf <jessicalealarf8@gmail.com>
2023-11-08 23:58:46 -0300

    feat: todo entity created

---
+++ src/main/java/br/com/todolist/todolist/entity/Todo.java
@@ -0,0 +1,25 @@
+package br.com.todolist.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "todo")
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+
+
+}
