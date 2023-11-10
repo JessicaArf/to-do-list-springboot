10f52ad81c92363c6453eccb85fc229df92226a4
Jessica Arf <jessicalealarf8@gmail.com>
2023-11-10 00:38:38 -0300

    feat: connection with db created

--- src/main/java/br/com/todolist/todolist/controller/TodoController.java
+++ src/main/java/br/com/todolist/todolist/controller/TodoController.java
@@ -27,12 +27,12 @@ public class TodoController {
 
 
     @PutMapping
-    List<Todo> update(Todo todo) {
+    List<Todo> update(@RequestBody Todo todo) {
         return todoService.update(todo);
     }
 
-    @DeleteMapping
-    List<Todo> delete(Long id) {
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
         return todoService.delete(id);
     }
 
